18422adb25fe227f01f47b1b990b64a652eb5326
mohamadsadeque <30416594+mohamadsadeque@users.noreply.github.com>
2018-09-14 11:12:29 -0300

    Update piezo.cpp

--- piezo.cpp
+++ piezo.cpp
@@ -1,25 +1,78 @@
 
 int d = 261;
-int r =  294;
+int r = 294;
 int m = 329;
 int f = 349; 
 int S = 392; 
 int l = 440; 
 int s = 493; 
 int D = 523; 
+
 int piezo = 3;
-char notes[] = "drmfslSD";
-int beats[] = {1,1,1,1,1,1,1,1};
+int pot = A0;
+int botao = 7;
+int tom;
+char notes[] = "drmfff drdrrr dSfmmm drmfff";
+
+
+//char notes[] = {'d','S','p','f','D','S','l','f'};
+int beats[] = {1, 1, 1, 2, 1, 1,1,1, 1, 1, 2, 1, 1,1,1,1,1,2,1,1,1,1,1,1,2,1,1,4};
 void setup() {
   // put your setup code here, to run once:
 pinMode(piezo, OUTPUT);
+pinMode(botao, INPUT_PULLUP);
+pinMode(pot, INPUT);
+Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  for(int i = 0; i<8 ; i++){
-    tone(piezo, notes[i], beats[i]*20 );
-    }
+if(!digitalRead(botao)){
+  tom = map(analogRead(pot),0,1023,261,523); 
+ 
+  tone(piezo, tom);
 
 }
+
+else
+noTone(piezo);
+
+}
+
+
+/*
+  for(int i = 0; i<sizeof(beats)/2 ; i++){
+switch (notes[i]){
+case 'd': tom =  261;
+break;
+case 'r': tom =  294;
+break;
+case 'm': tom =  329;
+break;
+case 'f': tom =  349;
+break;
+case 'S': tom =  392;
+break;
+case 'l': tom =  440;
+break;
+case 's': tom =  493;
+break;
+case 'D': tom =  523;
+break;
+case ' ': tom =  0;
+break;
+}
+    if(tom == 0){
+    noTone(piezo); 
+    }
+    else{
+    tone(piezo, tom);
+    
+    }
+    delay(beats[i]*200); 
+    noTone(piezo);
+  delay(40);
+  }
+*/
+
+ 
